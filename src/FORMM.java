58baedb17d3d722c6adc6b1ed62deec6e7af96a6
mreda4 <mariamreda278@gmail.com>
2025-04-27 15:46:08 +0300

    added RegistrationForm.form & RegistrationForm.java, a login image, and a label(Register)

---
+++ src/FORMM.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class FORMM {
+    private JTextField tfName;
+    private JTextField tfEmail;
+    private JTextField tfPhone;
+    private JTextField tfAddress;
+    private JTextField pfPassword;
+    private JButton btnregister;
+    private JButton btncencel;
+    private JTextField pfConfirmPassword;
+}
--- src/RegistrationForm.java
+++ src/RegistrationForm.java
@@ -1,2 +1,5 @@
+import javax.swing.*;
+
 public class RegistrationForm {
+    private JPanel panel1;
 }
